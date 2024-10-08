640b006a4b90abb82afa87084d371fc0b94b064c
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-08 15:59:05 +0800

    prebuilt: Update updater data directory

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
-include vendor/rising/config/rising.mk
+include vendor/mist/config/mist.mk
 
 PRODUCT_BRAND ?= LineageOS
 
